aaf02feb46c7d82b36abc0c0e87a0c2340d79293
talabidele <talabibamidele7@gmail.com>
2022-05-04 16:03:39 +0100

    changed all static pages

--- pages/about.js
+++ pages/about.js
@@ -12,3 +12,21 @@ const about = () => {
 };
 
 export default about;
+
+export async function getServerSideProps({ query: { slug }, req }) {
+  const resPost = await fetch(`${NEXT_PUBLIC_API_URL}/posts?slug=${slug}`);
+  const post = await resPost.json();
+
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/posts?comments_gte=3`);
+  const posts = await res.json();
+
+  const resComments = await fetch(`${NEXT_PUBLIC_API_URL}/comments`);
+  const comments = await resComments.json();
+
+  const resLikes = await fetch(`${NEXT_PUBLIC_API_URL}/likes`);
+  const likes = await resLikes.json();
+
+  return {
+    props: { post, posts, comments, likes },
+  };
+}
--- pages/contact.js
+++ pages/contact.js
@@ -13,3 +13,21 @@ const Contact = () => {
 };
 
 export default Contact;
+
+export async function getServerSideProps({ query: { slug }, req }) {
+  const resPost = await fetch(`${NEXT_PUBLIC_API_URL}/posts?slug=${slug}`);
+  const post = await resPost.json();
+
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/posts?comments_gte=3`);
+  const posts = await res.json();
+
+  const resComments = await fetch(`${NEXT_PUBLIC_API_URL}/comments`);
+  const comments = await resComments.json();
+
+  const resLikes = await fetch(`${NEXT_PUBLIC_API_URL}/likes`);
+  const likes = await resLikes.json();
+
+  return {
+    props: { post, posts, comments, likes },
+  };
+}
