9973df0dee69fed657479d5771755b8c18983802
talabidele <talabibamidele7@gmail.com>
2022-08-05 02:58:33 +0100

    Finished blog on home page

--- Components/BlogSection/Blogging.js
+++ Components/BlogSection/Blogging.js
@@ -2,9 +2,19 @@ import React from "react";
 import { Container } from "./style";
 import Link from "next/link";
 import Image from "next/image";
+import { AiOutlineClockCircle, AiOutlineCalendar } from "react-icons/ai";
+import { HiUserCircle } from "react-icons/hi";
+import Moment from "react-moment";
+import { useRouter } from "next/router";
 
 const Blogging = ({ blogPosts }) => {
   console.log(blogPosts);
+  const router = useRouter();
+
+  const handleLink = (e) => {
+    router.push(`/blog/${e.slug}`);
+  };
+
   return (
     <Container>
       <div className="container">
@@ -18,7 +28,7 @@ const Blogging = ({ blogPosts }) => {
         <div className="wrapper">
           {blogPosts.map((e) => (
             <div className="card" key={e.id}>
-              <div className="image">
+              <div className="image" onClick={() => handleLink(e)}>
                 <Image
                   src={e.image.url}
                   alt={e.image.name}
@@ -27,6 +37,26 @@ const Blogging = ({ blogPosts }) => {
                   objectFit="cover"
                   className="img"
                 />
+                <div className="other">
+                  <p>
+                    <AiOutlineClockCircle /> {e.minutes} min read
+                  </p>
+                  <div className="bg">
+                    <h3>{e.title}</h3>
+                    <div className="details">
+                      <div className="user">
+                        <HiUserCircle />
+                        {e.author}
+                      </div>
+                      <div className="time">
+                        <AiOutlineCalendar />
+                        <Moment format="D MMM YYYY" withTitle>
+                          {e.created_at}
+                        </Moment>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
--- Components/BlogSection/style.js
+++ Components/BlogSection/style.js
@@ -10,6 +10,7 @@ export const Container = styled.div`
     justify-content: space-between;
     color: #07036e;
     align-items: center;
+    margin-bottom: 2rem;
   }
 
   h1 {
@@ -21,7 +22,102 @@ export const Container = styled.div`
     font-size: 26px;
   }
 
+  img {
+    &:hover {
+      /* object-fit: cover; */
+      transform: scale(1.2);
+      transition: transform 1s, filter 2s ease-in-out;
+      filter: blur(2px);
+    }
+  }
+
   .img {
     border-radius: 15px;
+  }
+
+  div.image {
+    position: relative;
+    width: 400px;
+    height: 500px;
+    cursor: pointer;
+    transition: transform 0.2s, filter 0.3s ease-in-out;
+
+    &:hover {
+      /* object-fit: cover; */
+      /* transform: scale(1.2); */
+      /* transition: transform 0.1s, filter s ease-in-out; */
+      filter: blur(2px);
+    }
+
+    div.other {
+      position: absolute;
+      top: 0rem;
+      width: 100%;
+      height: 100%;
+
+      .bg {
+        background: linear-gradient(
+          180deg,
+          rgba(196, 196, 196, 0) 7.45%,
+          #020127 63.85%
+        );
+        width: 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 10rem;
+        position: absolute;
+        bottom: 0;
+        border-radius: 15px;
+      }
+
+      p {
+        color: #fff;
+        background: #020127;
+        width: 40%;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        position: absolute;
+        font-weight: 700;
+        padding: 10px 2px;
+        top: 0rem;
+        right: 0rem;
+        border-radius: 0px 15px 0px 0px;
+        justify-content: center;
+
+        svg {
+          font-weight: 700;
+        }
+      }
+
+      h3 {
+        position: absolute;
+        bottom: 5rem;
+        color: #fff;
+        width: 70%;
+        margin: auto;
+        left: 2rem;
+        font-size: 26px;
+
+        /* top: 0rem; */
+      }
+
+      .details {
+        position: absolute;
+        bottom: 2rem;
+        display: flex;
+        color: #fff;
+        font-weight: 500;
+        left: 2rem;
+        gap: 1rem;
+
+        .user,
+        .time {
+          align-items: center;
+          display: flex;
+          gap: 10px;
+        }
+      }
+    }
   }
 `;
