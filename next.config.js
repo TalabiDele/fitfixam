745cd4eba5475a448cdc4e3fe9f520cac840c029
talabidele <talabibamidele7@gmail.com>
2022-09-21 02:55:19 +0100

    Added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   future: { webpack5: true },
   reactStrictMode: true,
   images: {
@@ -7,4 +11,4 @@ module.exports = {
   env: {
     NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
   },
-};
+});
