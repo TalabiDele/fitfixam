73a6513e3c96682ab381b2af34ad274c4c2a96c8
talabidele <talabibamidele7@gmail.com>
2022-09-21 01:15:41 +0100

    Added dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import Layout from "@/components/Layout";
-import Hero from "Components/HeroSection/Hero";
-import Review from "Components/ReviewSection/Review";
-import Action from "Components/Action/Action";
-import Subscribe from "Components/Subscribe/Subscribe";
-import SecondAction from "Components/Action/SecondAction";
-import Conversation from "@/components/Conversation/Conversation";
-import Find from "@/components/Find/Find";
-import Blogging from "@/components/BlogSection/Blogging";
-import styled from "styled-components";
+// import Hero from "Components/HeroSection/Hero";
+// import Review from "Components/ReviewSection/Review";
+// import Action from "Components/Action/Action";
+// import Subscribe from "Components/Subscribe/Subscribe";
+// import SecondAction from "Components/Action/SecondAction";
+// import Conversation from "@/components/Conversation/Conversation";
+// import Find from "@/components/Find/Find";
+// import Blogging from "@/components/BlogSection/Blogging";
 import { NEXT_PUBLIC_API_URL } from "@/config/index";
 
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
 export default function Home({ posts, blogPosts }) {
+  const Hero = dynamic(() => import("Components/HeroSection/Hero"));
+  const Review = dynamic(() => import("Components/ReviewSection/Review"));
+  const Action = dynamic(() => import("Components/Action/Action"));
+  const Subscribe = dynamic(() => import("Components/Subscribe/Subscribe"));
+  const SecondAction = dynamic(() => import("Components/Action/SecondAction"));
+  const Conversation = dynamic(() =>
+    import("@/components/Conversation/Conversation")
+  );
+  const Find = dynamic(() => import("@/components/Find/Find"));
+  const Blogging = dynamic(() => import("@/components/BlogSection/Blogging"));
+
   return (
-    <div>
+    <>
       <Layout
         title="Fitfixam | Home"
         description="Here, you can find reviews of artisans and companies that offer artisan services. Be girded with the information that will help you make the right decisions. Fix it one time"
@@ -41,7 +54,7 @@ export default function Home({ posts, blogPosts }) {
         <Subscribe />
         <SecondAction />
       </Layout>
-    </div>
+    </>
   );
 }
 
